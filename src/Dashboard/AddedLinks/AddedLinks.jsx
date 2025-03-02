d7938242996d1310a9196132f4c31d6d62da691d
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-03 05:38:42 +0600

    Added links showing

---
+++ src/Dashboard/AddedLinks/AddedLinks.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AddedLinks = () => {
+    return (
+        <div className='w-full p-6 bg-white border border-orange-200 shadow-md'>
+            <ol>
+                <li>www.facebook.com/kt</li>
+                <li>www.facebook.com/kt</li>
+                <li>www.facebook.com/kt</li>
+                <li>www.facebook.com/kt</li>
+            </ol>
+        </div>
+    );
+};
+
+export default AddedLinks;
--- src/Dashboard/Home/Home.jsx
+++ src/Dashboard/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddSocialLinks from '../AddLinks/Social/AddSocialLinks';
 import AddOwnLinks from '../AddLinks/OwnLinks/AddOwnLinks';
+import AddedLinks from '../AddedLinks/AddedLinks';
 
 const Home = () => {
     return (
@@ -12,6 +13,10 @@ const Home = () => {
             <div className='w-[49%] h-72 bg-white rounded-[7px] shadow-sm border border-white hover:border-orange-200 duration-500 p-3'>
                 <AddOwnLinks/>
             </div>
+            </div>
+            <div className='w-full flex items-center justify-between my-6'>
+            <AddedLinks/>
+            <div></div>
             </div>
         </div>
     );
