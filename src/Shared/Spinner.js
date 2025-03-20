f70f4ae76e8b4126f86924abba3543c0822e0528
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-20 20:21:43 +0600

    spinner changed

--- src/Shared/Spinner.js
+++ src/Shared/Spinner.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Blocks } from 'react-loader-spinner';
+import { InfinitySpin } from 'react-loader-spinner';
 
 const Spinner = () => {
     return (
         <div className="flex justify-center items-center h-[85vh] bg-white">
-            <Blocks
-                height="180"
-                width="180"
-                color={["#f97316"]}
-                ariaLabel="blocks-loading"
-                wrapperClass="blocks-wrapper"
-                visible={true}
+            <InfinitySpin
+            visible={true}
+            width="200"
+            color="#f97316"
+            ariaLabel="infinity-spin-loading"
             />
         </div>
     );
