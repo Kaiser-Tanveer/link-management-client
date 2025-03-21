50dbc094a4f6811be2e3f2d69df9e26e73801ace
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-21 21:13:04 +0600

    link list updated

--- src/Dashboard/AddedLinks/MyOwnLinks.jsx
+++ src/Dashboard/AddedLinks/MyOwnLinks.jsx
@@ -22,12 +22,12 @@ const MyOwnLinks = () => {
         <div>
     <h2 className="text-xl font-semibold text-center">My Own Links</h2>
     {links?.data?.map((link, index) => (
-        <article key={index} className="mb-4 flex items-center gap-6">
-            <h4 className='font-bold underline'>{link?.name}:</h4>
-            <hr className='border-2 h-full w-full' />
+        <article key={index} className="mb-4 flex items-center">
+            <h4 className='font-bold underline w-20'>{link?.name}:</h4>
+            <hr className='border h-full w-6 border-orange-500' />
             <ul>
                 {link?.links?.map((item, idx) => (
-                    <li key={idx} className='flex items-center gap-2'>
+                    <li key={idx} className='flex items-center gap-2 border-l-2 pl-2 border-orange-500'>
                         <p>{idx + 1}.</p>
                         <a href={item} target="_blank" rel="noopener noreferrer" className="flex items-center link-hover text-orange-500">
                             {item?.length > 30 ? `${item?.slice(0, 30)}...` : item}
