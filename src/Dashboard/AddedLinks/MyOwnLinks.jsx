6b7ff72817c82025079408407f4ade164a14c306
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-22 12:53:32 +0600

    get social link is done

--- src/Dashboard/AddedLinks/MyOwnLinks.jsx
+++ src/Dashboard/AddedLinks/MyOwnLinks.jsx
@@ -20,25 +20,25 @@ const MyOwnLinks = () => {
     }, []);
     return (
         <div>
-    <h2 className="text-xl font-semibold text-center">My Own Links</h2>
-    {links?.data?.map((link, index) => (
-        <article key={index} className="mb-4 flex items-center">
-            <h4 className='font-bold underline w-20'>{link?.name}:</h4>
-            <hr className='border h-full w-6 border-orange-500' />
-            <ul>
-                {link?.links?.map((item, idx) => (
-                    <li key={idx} className='flex items-center gap-2 border-l-2 pl-2 border-orange-500'>
-                        <p>{idx + 1}.</p>
-                        <a href={item} target="_blank" rel="noopener noreferrer" className="flex items-center link-hover text-orange-500">
-                            {item?.length > 30 ? `${item?.slice(0, 30)}...` : item}
-                            <FaLink className="ml-2" />
-                        </a>
-                    </li>
-                ))}
-            </ul>
-        </article>
-    ))}
-</div>
+            <h2 className="text-xl font-semibold text-center">My Own Links</h2>
+            {links?.data?.map((link, index) => (
+                <article key={index} className="mb-4 flex items-center">
+                    <h4 className='font-bold underline w-20'>{link?.name}:</h4>
+                    <hr className='border h-full w-6 border-orange-500' />
+                    <ul>
+                        {link?.links?.map((item, idx) => (
+                            <li key={idx} className='flex items-center gap-2 border-l-2 pl-2 border-orange-500'>
+                                <p>{idx + 1}.</p>
+                                <a href={item} target="_blank" rel="noopener noreferrer" className="flex items-center link-hover text-orange-500">
+                                    {item?.length > 30 ? `${item?.slice(0, 30)}...` : item}
+                                    <FaLink className="ml-2" />
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </article>
+            ))}
+        </div>
 
     );
 };
--- src/Dashboard/AddedLinks/MySocialLinks.jsx
+++ src/Dashboard/AddedLinks/MySocialLinks.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { FaLink } from 'react-icons/fa';
 
 const MySocialLinks = () => {
+    const [links, setLinks] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    useEffect(() => {
+            axios.get('http://localhost:5000/api/links/get-social')
+                .then(response => {
+                    setLinks(response.data);
+                    setLoading(false);
+                })
+                .catch(error => {
+                    setError(error.message);
+                    setLoading(false);
+                });
+        }, []);
     return (
         <div>
-        <h2 className='text-xl font-semibold text-center'>My Social Links</h2>
-            <ol>
-                <li className='flex items-center gap-2 link-hover cursor-pointer'><FaLink/> www.facebook.com/kt</li>
-                <li className='flex items-center gap-2 link-hover cursor-pointer'><FaLink/>www.facebook.com/kt</li>
-                <li className='flex items-center gap-2 link-hover cursor-pointer'><FaLink/>www.facebook.com/kt</li>
-                <li className='flex items-center gap-2 link-hover cursor-pointer'><FaLink/>www.facebook.com/kt</li>
-            </ol>
-        </div>
+        <h2 className="text-xl font-semibold text-center">My Social Links</h2>
+        {links?.data?.map((link, index) => (
+            <article key={index} className="mb-4 flex items-center">
+                <h4 className='font-bold underline w-20'>{link?.name}:</h4>
+                <hr className='border h-full w-6 border-orange-500' />
+                <ul>
+                    {link?.links?.map((item, idx) => (
+                        <li key={idx} className='flex items-center gap-2 border-l-2 pl-2 border-orange-500'>
+                            <p>{idx + 1}.</p>
+                            <a href={item} target="_blank" rel="noopener noreferrer" className="flex items-center link-hover text-orange-500">
+                                {item?.length > 30 ? `${item?.slice(0, 30)}...` : item}
+                                <FaLink className="ml-2" />
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </article>
+        ))}
+    </div>
     );
 };
 
