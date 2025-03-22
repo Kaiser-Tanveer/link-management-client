4688398141cc31e8ea630cd131f025b475d1ada4
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-22 13:21:24 +0600

    social links generated successfully

--- src/Dashboard/AddedLinks/MySocialLinks.jsx
+++ src/Dashboard/AddedLinks/MySocialLinks.jsx
@@ -9,7 +9,7 @@ const MySocialLinks = () => {
     useEffect(() => {
             axios.get('http://localhost:5000/api/links/get-social')
                 .then(response => {
-                    setLinks(response.data);
+                    setLinks(response.data.data);
                     setLoading(false);
                 })
                 .catch(error => {
@@ -17,23 +17,22 @@ const MySocialLinks = () => {
                     setLoading(false);
                 });
         }, []);
+        console.log(links);
     return (
         <div>
         <h2 className="text-xl font-semibold text-center">My Social Links</h2>
-        {links?.data?.map((link, index) => (
+        {links?.map((link, index) => (
             <article key={index} className="mb-4 flex items-center">
                 <h4 className='font-bold underline w-20'>{link?.name}:</h4>
                 <hr className='border h-full w-6 border-orange-500' />
                 <ul>
-                    {link?.links?.map((item, idx) => (
-                        <li key={idx} className='flex items-center gap-2 border-l-2 pl-2 border-orange-500'>
-                            <p>{idx + 1}.</p>
-                            <a href={item} target="_blank" rel="noopener noreferrer" className="flex items-center link-hover text-orange-500">
-                                {item?.length > 30 ? `${item?.slice(0, 30)}...` : item}
+                        <li className='flex items-center gap-2 border-l-2 pl-2 border-orange-500'>
+                            <p>{index + 1}.</p>
+                            <a href={link?.url} target="_blank" rel="noopener noreferrer" className="flex items-center link-hover text-orange-500">
+                                {link?.url?.length > 30 ? `${link?.url?.slice(0, 30)}...` : link?.url}
                                 <FaLink className="ml-2" />
                             </a>
                         </li>
-                    ))}
                 </ul>
             </article>
         ))}
