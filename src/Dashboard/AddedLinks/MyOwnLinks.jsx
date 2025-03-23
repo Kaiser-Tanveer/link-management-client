9cba4e2e4cba2795d7191d09372fba8fff716120
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-24 00:32:08 +0600

    own link responsive is done

--- src/Dashboard/AddedLinks/MyOwnLinks.jsx
+++ src/Dashboard/AddedLinks/MyOwnLinks.jsx
@@ -23,9 +23,10 @@ const MyOwnLinks = () => {
             <h2 className="text-xl font-semibold text-center">My Own Links</h2>
             {links?.data?.map((link, index) => (
                 <article key={index} className="mb-4 flex items-center">
-                    <h4 className='font-bold underline w-20'>{link?.name}</h4>
-                    <hr className='border h-full w-10 border-orange-500' />
-                    <ul>
+
+                    <h4 className='font-bold w-20'>{link?.name}</h4>
+                    <hr className='hidden md:block border h-full w-10 border-orange-500' />
+                    <ul className='w-[80%] md:w-full'>
                         {link?.links?.map((item, idx) => (
                             <li key={idx} className='flex items-center gap-2 border-l-2 pl-2 border-orange-500'>
                                 <p>{idx + 1}.</p>
