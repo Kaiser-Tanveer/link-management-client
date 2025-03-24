2fc8f60b2713786e371a285f077f6087f2405ca3
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-24 15:05:40 +0600

    aligned the add link btn

--- src/Dashboard/AddLinks/OwnLinks/AddOwnLinks.jsx
+++ src/Dashboard/AddLinks/OwnLinks/AddOwnLinks.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 import OwnLinkModal from './OwnLinkModal';
+import { FaPlusCircle } from 'react-icons/fa';
 
 const AddOwnLinks = () => {
     const [open, setOpen] = useState(false);
     return (
         <div className='w-full'>
             <h2 className='text-xl font-semibold text-center'>Add own links</h2>
-            <div className='bg-white mt-4 gap-6'>
-                <button onClick={() => setOpen(true)} className='border border-orange-500 text-orange-500 rounded-[7px] px-2 py-1'>
+            <div className='flex justify-center mt-4'>
+                <button 
+                    onClick={() => setOpen(true)} 
+                    className='flex items-center gap-2 border border-orange-500 text-orange-500 rounded-[7px] px-3 py-2'
+                >
+                    <FaPlusCircle className='text-orange-500 text-xl'/>
                     Click to add
                 </button>
             </div>
-            {
-                open &&
-                <OwnLinkModal
-                    setOpen={setOpen}
-                />
-            }
+            {open && <OwnLinkModal setOpen={setOpen} />}
         </div>
     );
 };
