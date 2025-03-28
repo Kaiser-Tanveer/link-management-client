ad93feadc9db8e4ce54a4f8f95056d566562d0ce
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-28 13:10:48 +0600

    menu added to header

--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -7,14 +7,16 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     const [profile, setProfile] = useState(false);
     return (
-        <>
-        <div onClick={() => profile && setProfile(false)} className='bg-orange-500 py-1 text-white px-2'>
-            <Link className='text-left flex items-center h-10 hover:bg-orange-100 px-4 rounded-[7px] duration-500 text-orange-500'>
-                <FaLink className='mr-2 text-md'/>
-                <span>My Links</span>
-            </Link>
-            <Link className='flex items-center justify-end'><HiUserCircle onClick={() => setProfile(!profile)} className='text-4xl mr-2'/></Link>
-        </div>
+        <div className='bg-orange-500 py-1 text-white'>
+            <div className='flex items-center justify-between pl-8'>
+                <Link className='flex items-center'>
+                    <FaLink className='mr-2 text-md'/>
+                    <span>My Links</span>
+                </Link>
+                <div onClick={() => profile && setProfile(false)} className='px-2'>
+                    <Link className='flex items-center justify-end'><HiUserCircle onClick={() => setProfile(!profile)} className='text-4xl mr-2'/></Link>
+                </div>
+            </div>
         {
             profile &&
             <div className='absolute right-4 w-32 border border-orange-200 shadow-md p-2 rounded-[7px] flex flex-col items-start justify-start bg-white z-[100]'>
@@ -22,7 +24,7 @@ const Header = () => {
             <Link className='flex items-center justify-start h-10 hover:bg-orange-200 w-full px-2 rounded-[7px]'><HiOutlineLogout className='text-xl mr-2'/> <span>Logout</span></Link>
             </div>
         }
-        </>
+        </div>
     );
 };
 
